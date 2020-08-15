32982979a72499f45fd416b608c37fd14d9377c1
Tatyana <hanyatan@gmail.com>
2020-08-14 22:28:18 -0400

    handlebars_layouts_profilePage

---
+++ public/js/profilePage.js
@@ -0,0 +1,42 @@
+let bootCamps = [
+    {
+    name: "Columbia Engineering Boot Camps",
+    link: "https://bootcamp.cvn.columbia.edu/?utm_source=coursereport&utm_medium=schoolpage"
+},
+{
+    name: "Flatiron School",
+    link: "https://flatironschool.com/?utm_source=coursereport&utm_medium=schoolpage"
+},
+{
+    name: "Hack Reactor",
+    link: "https://www.hackreactor.com/coding-bootcamp"
+},
+{
+    name: "Codesmith",
+    link: "https://www.codesmith.io"
+},
+{
+    name: "App Academy",
+    link: "https://www.appacademy.io"
+},
+{
+    name: "Turing",
+    link: "https://turing.io"
+},
+{
+    name: "Fullstack Academy",
+    link: "https://www.fullstackacademy.com"
+},
+{
+    name: "General Assembly",
+    link: "https://generalassemb.ly"
+},
+{
+    name: "Tech Elevator",
+    link: "https://www.techelevator.com"
+},
+{
+    name: "Coder Academy",
+    link: "https://coderacademy.edu.au/?utm_source=coursereport&utm_medium=schoolpage"
+}
+]
