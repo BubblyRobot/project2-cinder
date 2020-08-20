7b4697229d4cb0a594c7b92fff953d67a5cdc6d2
BubblyRobot <susyjustice@gmail.com>
2020-08-19 23:24:30 -0400

    added google api stuff

--- public/js/api.js
+++ public/js/api.js
@@ -1,10 +1,16 @@
 
 var searchBtn = $("#search-button");
+require("dotenv").config(process.env.GOOGLE_API_KEY);
+
+const api_key = process.env.GOOGLE_API_KEY;
+
+
+
 console.log('we loaded js file!!!')
 function campusLocation(city){
     
    
-    var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&=AIzaSyBN4HIhuB3jg44HRs46sZvsgD22Z0mY6Uk";
+    var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${api_key}`;
 
 
 
