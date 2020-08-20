577ec9df4bb48191933c4e5a2337e069fb82fceb
BubblyRobot <susyjustice@gmail.com>
2020-08-20 00:11:39 -0400

    Api Test

--- public/js/api.js
+++ public/js/api.js
@@ -1,16 +1,14 @@
 
 var searchBtn = $("#search-button");
-require("dotenv").config(process.env.GOOGLE_API_KEY);
-
-const api_key = process.env.GOOGLE_API_KEY;
-
-
-
 console.log('we loaded js file!!!')
+
+//const appApi = require("dotenv").config(process.env.PRODUCTION);
+//const PRODUCTION = process.env.PRODUCTION;
+
 function campusLocation(city){
     
    
-    var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${api_key}`;
+    var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${PRODUCTION}`;
 
 
 
