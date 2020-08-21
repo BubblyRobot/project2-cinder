9d69fde959e00a1ca46edc26a425a049c7672c09
BubblyRobot <susyjustice@gmail.com>
2020-08-21 13:49:55 -0400

    Final Day Changes

--- public/js/api.js
+++ public/js/api.js
@@ -1,29 +1,48 @@
 
 var searchBtn = $("#search-button");
 console.log('we loaded js file!!!')
+//
 
+<<<<<<< HEAD
 const appApi = require("dotenv").config(process.env.GOOGLE_API_KEY);
 //const PRODUCTION = process.env.PRODUCTION;
+=======
+// backend interal call db 
+// routes backend and call the external api and return teh data to the front
+>>>>>>> e8b252f2cbb0c18ffe82de2cf79f62a9bb37ad8d
 
-function campusLocation(city){
+
+function campusLocation(){
     
+<<<<<<< HEAD
    
     var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${GOOGLE_API_KEY}`;
+=======
+ var queryURL = "/api/place/"
+  //  var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${PRODUCTION}`;
+>>>>>>> e8b252f2cbb0c18ffe82de2cf79f62a9bb37ad8d
 
 
+  $("#saveBut").click(function(){
+    console.log("plz work")
+    console.log($("#usersInput").val())
 
     $.ajax({
+        data:{
+            userInput: $("#usersInput").val()
+        },
         url: queryURL,
         method: "GET"
     }).then(function (response) {
 
-        console.log(response)
-
-        
-
-
+        console.log(response.name)
+        $("#location").text(JSON.stringify(response.formatted_address));
+        $("#tyler").text(JSON.stringify(response.name));
     }).catch(function(err){
         console.log('ERR!!!',err)
     })
+
+  })
+
 }
 campusLocation()
--- utils/users.js
+++ utils/users.js
@@ -1,38 +1,36 @@
-// Connect to database here. 
 const users = [];
 
-// Join user to chat every user needs a unique id
-function userJoin(id, username, room){
-    const user = { id, username, room };
+// Join user to chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
 
-    users.push(user);
+  users.push(user);
 
-    return user; 
+  return user;
 }
 
-// get current user
+// Get current user
 function getCurrentUser(id) {
-    return users.find(user => user.id === id);
+  return users.find(user => user.id === id);
 }
 
-// User leasves hat
+// User leaves chat
 function userLeave(id) {
-    const index = users.findIndex(user => user.id === id);
+  const index = users.findIndex(user => user.id === id);
 
-    if(index != -1){
-        return users.splice(index, 1)[0];
-    }
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
 }
-
 
 // Get room users
-function getRoomUsers(){
-    return users.filter(user => user.room === room);
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
 }
 
 module.exports = {
-    userJoin,
-    getCurrentUser,
-    userLeave,
-    getRoomUsers
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
 };
