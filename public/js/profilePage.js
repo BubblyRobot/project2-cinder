dfa190e07191499bcde5e560e3f53e2700f946bf
nargizaboronchieva <64052876+nargizaboronchieva@users.noreply.github.com>
2020-08-21 15:04:30 -0400

    Update profilePage.js
    updated list of free bootcamps

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -1,44 +1,123 @@
-let bootCamps = [
-    {
-    name: "Columbia Engineering Boot Camps",
-    link: "https://bootcamp.cvn.columbia.edu/?utm_source=coursereport&utm_medium=schoolpage"
+let freeBootCamps = [
+{
+    name: "42",
+    link: "www.42.us.org",
+    programm: "42 is a free coding bootcamp in Paris and Silicon Valley. The students must complete a four-week ‘Piscine’ coding challenge before attending the full-time program, covering subjects in full stack web development, data visualization, cybersecurity, and more. There are no instructors at 42; instead, students learn through their peers and mentors who are tech industry professionals.",
+    available: "Full-time",
+},  
+{
+    name: "About Ada Developers Academy",
+    link: "adadevelopersacademy.org",
+    programm: "Ada Developers Academy is a non-profit, tuition-free coding school for women and gender diverse adults in downtown Seattle. Aptly named after the first programmer, Ada Lovelace, they focus on serving people with low incomes, underrepresented minorities, and members of the LGBTQIA+ community. Their full stack web development program allows students to choose what specialty of software development they want to pursue in their post-Ada careers, covering HTML/CSS, JavaScript, Ruby on Rails, and Computer Science fundamentals. Their complete curriculum is online and open-source",
+    available: "Full-time",
+}, 
+{
+    name: "App Academy",
+    link: "www.appacademy.io",
+    programm: "App Academy is a coding school that offers online and in-person training programs with no tuition cost until you’re hired as a Software Engineer earning over $50,000. The career changing outcomes that App Academy has produced since being founded in 2012 is a major reason why it’s considered the most respected code school in the industry.",
+    available: "Online, Full-time, Self-paced",
+},
+{
+    name: "Bento",
+    link: "https://bento.io",
+    programm: "ABento.io takes an interesting approach to free courses in coding and web development. Each “track” is a mini-course compiled from learning resources from around the web. It’s a great approach that allows you to learn in different ways and from many different sources. Bento offers tracks in CSS, HTML, JavaScript, Git, Python, jQuery, and more. It also has more comprehensive tracks like those for front end development, backend development, and web fundamentals.",
+    available: "Self-paced",
+}, 
+{
+    name: "Bridge",
+    link: "https://bridgeschool.iog",
+    programm: "Bridge is a women’s coding bootcamp that offers an 11-week software development course. Students work on a wide range of portfolio projects. These projects are designed to impart real-world experience. This women’s bootcamp helps build technical skills. It offers a supportive and high-tech environment. Bridge is an in-person bootcamp, offering classes 2 times per week. The instructors are industry experts.",
+    available: "Full-time",
+},  
+{
+    name: "Code Academy",
+    link: "https://www.codecademy.com/",
+    programm: "CodeCademy is a well-known free bootcamp. This online program is popular with beginners and intermediate coders. Students learn coding languages like HTML and JavaScript. Students work on projects to improve their skills and build a portfolio. CodeCademy’s free courses offer a chance to learn programming at no cost.",
+available: "Full-time",
+},
+{
+    name: "Code Wars",
+    link: "https://www.codewars.com/dashboard",
+    programm: "Codewars is an outstanding and innovative way to learn how to code. The program involves challenges, which they call “kata”, in which you code with other users to learn programming languages like Java, JavaScript, Python, Ruby, and many others.",
+    available: "Online",
+},
+{
+    name: "Coursera",
+    link: "Coursera is a unique free coding bootcamp. Classes at this bootcamp are taught by university professors. And while the courses are free, there are some small charges involved. This popular bootcamp offers certificates, which cost between around $30 and $50. The certificates verify course completion and make a welcome addition to a portfolio.",
+    programm: "https://www.coursera.org/",
+    available: "Online",
+},
+{
+    name: "THE DATA INCUBATOR",
+    link: "https://www.thedataincubator.com/",
+    programm: "The Data Incubator offers a free coding bootcamp for experienced programmers. The curriculum is designed to equip these individuals with the final important skills to become true seasoned professionals. The program is a fellowship, free for students. Employers pay a fee if they hire a fellow.",
+    available: "Online, Full-time",
+},
+{
+    name: "edX",
+    link: "https://www.edx.org/",
+    programm: "edX, like Coursera, offers hundreds of massive open online courses (MOOCs) on every subject under the sun. But their tech courses are especially great. On edX, you can take free courses in HTML5, CSS, mobile application development, Java Programming, and much more–all from top university instructors.",
+    available: "Online, Full-time, Part-time, Self-paced",
 },
 {
     name: "Flatiron School",
-    link: "https://flatironschool.com/?utm_source=coursereport&utm_medium=schoolpage"
+    link: "flatironschool.com",
+    programm: "Flatiron School is a big-name coding bootcamp. With locations in London, New York City, and Washington D.C., students can attend programming courses in-person or online. And their introductory courses are completely free.",
+    available: "Online, Full-time, Part-time, Self-paced",
+},
+ 
+{
+    name: "Founders and Coders",
+    link: "foundersandcoders.com",
+    programm: "Founders and Coders is a UK-based non-profit organization offering tuition-free and peer-led web development training. Their 16-week full-time coding bootcamp covers JavaScript, Node.js, relational databases, and aspects of UX design and project management. They are a community founded on the principle of reciprocity and expect all their graduates to contribute to the program, starting with mentoring for at least one week after they graduate. At this time, Coders and Founders are only accepting people who have the right to work in the UK and who intend to seek employment in London after completing the program. ",
+    available: "Full-time",
 },
 {
-    name: "Hack Reactor",
-    link: "https://www.hackreactor.com/coding-bootcamp"
-},
-{
-    name: "Codesmith",
-    link: "https://www.codesmith.io"
-},
-{
-    name: "App Academy",
-    link: "https://www.appacademy.io"
-},
-{
-    name: "Turing",
-    link: "https://turing.io"
+    name: "freeCodeCamp",
+    link: "https://www.freecodecamp.org/news/the-ultimate-bootcamp-primer/",
+    programm: "Free Code Camp offers coding certificates in front end and full stack development. This online program is self-paced and offers a 1,600-hour JavaScript curriculum. Demographically, Free Code Camp stands out. Roughly 80% of its coding community is over 25 years old, and 20% of its students are women.",
+    available: "Online, Full-time, Part-time, Self-paced",
 },
 {
     name: "Fullstack Academy",
-    link: "https://www.fullstackacademy.com"
+    link: "www.fullstackacademy.com",
+    programm: "Fullstack teaches an advanced, relevant curriculum that is constantly updated based on current tech trends. The course is focused on JavaScript-oriented technologies to prepare students for top development jobs at tech firms and startups alike. During the program, students will learn fundamentals, fullstack web development, best practices and tools, and the developer culture. Students will become comfortable and efficient using technologies that include JavaScript, HTML5, CSS3, Node.js, React, PostgreSQL, and Git. Students will also be exposed to industry practices including test-driven development and pair programming. They are also encouraged to expand their education with Fullstack’s CTO lecture series, which provides training for tech leadership and career development. In addition to the full-time, 17-week program, it offers various courses to fit students’ needs, including a bootcamp prep course designed to help students pass rigorous bootcamp admissions (students can choose between month- and week-long courses), and a 26-week Flex immersive course that offers an opportunity to complete the rigorous Fullstack Immersive program over a six-month period while maintaining a full-time job. Fullstack’s Bootcamp Prep will help prepare you for admissions into our prestigious flagship Software Engineering Immersive, Remote Immersive, and Flex Immersive programs. This rigorous course will also prepare you for admissions into other highly-ranked coding bootcamps. You’ll learn more than just the fundamentals of programming, you’ll learn how to solve real-world coding problems using the JavaScript language",
+    available: "Online, Full-time, Part-time, Self-paced",
 },
+
 {
     name: "General Assembly",
-    link: "https://generalassemb.ly"
+    link: "generalassemb.ly",
+    programm: "General Assembly offers a free coding bootcamp called GA Dash. This program is project-based. For example, students build an entire Tumblr theme from the ground-up. This unique program is popular, and comes from a top paid coding bootcamp.",
+    available: "Online, Full-time, Part-time, Self-paced",
+},
+
+{
+    name: "Hack Reactor Basic Prep",
+    link: "getcoding.hackreactor.com",
+    programm: "Hack Reactor, a well-known coding bootcamp, offers a free program called Hack Reactor Basic Prep. The course is as it sounds—an introductory course designed for beginners. The program runs for 12 weeks and covers all the basics of coding.",
+    available: "Online, Full-time, Part-time, Self-paced",
+},
+    
+{
+    name: "Insight Data Science",
+    link: "www.insightdatascience.com",
+    programm: "Insight offers a seven-week full-time post-doctoral training fellowship in Silicon Valley and New York. Their program fosters collaboration and focuses on building real-world data science solutions. Insight Data Science only accepts applicants who have completed or are approaching completion of a PhD.",
+    available: "Online, Full-time, Part-time, Self-paced",
 },
 {
-    name: "Tech Elevator",
-    link: "https://www.techelevator.com"
+    name: "Kenzie Academy",
+    link: "www.kenzie.academy",
+    programm: "Kenzie Academy offers a one-year full-time program in Software Engineering and UX Engineering in Indianapolis or online. Their software engineering program covers HTML, CSS, JavaScript, SQL, React, Python, and Django. Their UX engineering program teaches user-driven design with front end coding.",
+    available: "Online, Full-time, Part-time, Self-paced",
 },
 {
-    name: "Coder Academy",
-    link: "https://coderacademy.edu.au/?utm_source=coursereport&utm_medium=schoolpage"
+    name: "Khan Academy",
+    link: "https://www.khanacademy.org",
+    programm: "Khan Academy courses are very popular. They’re not a coding bootcamp, but this free education source offers a long list of free coding courses. Students can sharpen their computer skills with Khan Academy, and learn a lot on the way.",
+    available: "Online, Full-time, Part-time, Self-paced",
 }
+
 ]
 $(document).ready(function() {
 
