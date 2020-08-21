f4b550d1740639bcdfa307e4f368dc39567f6ee5
BubblyRobot <susyjustice@gmail.com>
2020-08-21 14:16:57 -0400

    Testing for Profile Stuff

--- public/js/api.js
+++ public/js/api.js
@@ -3,24 +3,28 @@ var searchBtn = $("#search-button");
 console.log('we loaded js file!!!')
 //
 
-<<<<<<< HEAD
 const appApi = require("dotenv").config(process.env.GOOGLE_API_KEY);
 //const PRODUCTION = process.env.PRODUCTION;
-=======
+
 // backend interal call db 
 // routes backend and call the external api and return teh data to the front
->>>>>>> e8b252f2cbb0c18ffe82de2cf79f62a9bb37ad8d
+
 
 
 function campusLocation(){
     
-<<<<<<< HEAD
-   
-    var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${GOOGLE_API_KEY}`;
-=======
  var queryURL = "/api/place/"
   //  var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${PRODUCTION}`;
->>>>>>> e8b252f2cbb0c18ffe82de2cf79f62a9bb37ad8d
+
+
+// backend interal call db 
+// routes backend and call the external api and return teh data to the front
+
+function campusLocation(){
+    
+ var queryURL = "/api/place/"
+  //  var queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=pizza%20new%20york&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${PRODUCTION}`;
+
 
 
   $("#saveBut").click(function(){
